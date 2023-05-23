cd0234a072beb7be52a1f230e06ef125a738803c
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-05-23 11:02:34 -0300

    Add Copyright

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -45,11 +45,12 @@ const Home = () => {
     for (let combination of winningCombinations) {
       const [a, b, c] = combination;
       if (board[a] === player && board[b] === player && board[c] === player) {
-        setWinner(player);
         if(player === 'X') {
           setScorePlayer1(scorePlayer1 + 1);
+          setWinner(player1);
         } else {
           setScorePlayer2(scorePlayer2 + 1);
+          setWinner(player2);
         }
         return;
       }
@@ -99,7 +100,7 @@ const Home = () => {
         )}
         {currentPlayer && !winner && (
           <>
-            <div className="flex flex-row absolute top-0 bg-gray-300 rounded-b-[20px] px-10">
+            <div className="flex flex-row absolute top-0 bg-gray-50 rounded-b-[20px] px-10 border border-gray-200">
               <h2 className="text-xl mb-2 text-gray-500 font-medium me-10">
                 {player1}:
                 <span className="ms-2 text-blue-500">{scorePlayer1}</span>
@@ -109,16 +110,16 @@ const Home = () => {
                 <span className="ms-2 text-blue-500">{scorePlayer2}</span>
               </h2>
             </div>
-            <div className="flex flex-col p-10 bg-gray-50 rounded-lg border border-gray-200 board">
-              <h2 className="text-xl mb-2 text-gray-500 font-medium">
+            <div className="flex flex-col p-10 bg-gray-50 rounded-lg border border-gray-200 board mt-10">
+              <h2 className="text-xl mb-2 text-gray-500 font-medium text-center">
                 Vez do Jogador:
                 <span className="ms-2 text-blue-500">{currentPlayer}</span>
               </h2>
-              <div className="grid grid-cols-3 w-[150px] mx-auto">
+              <div className="grid grid-cols-3 w-[300px] mx-auto">
                 {board.map((square, index) => (
                   <button
                     key={index}
-                    className="bg-gray-200 text-md font-bold h-[50px] w-[50px] rounded-md	text-center text-blue-500"
+                    className="text-5xl font-bold h-[100px] w-[100px] rounded-md	text-center text-blue-500 border border-gray-300 transition-all duration-700 hover:bg-blue-500 hover:text-white"
                     onClick={() => handleSquareClick(index)}
                   >
                     {square}
@@ -133,12 +134,12 @@ const Home = () => {
         {winner && (
           <div>
             {winner === 'Draw' ? (
-              <h2>It's a Draw!</h2>
+              <h2>É um empate!</h2>
             ) : (
-              <h2>{winner} Wins!</h2>
+              <h2>{winner} Ganhou !</h2>
             )}
             <button
-              className="p-2 bg-blue-500 text-white rounded mt-4"
+              className="p-2 bg-blue-500 hover:bg-blue-600 text-white rounded transition-all duration-700 mt-4"
               onClick={() => resetData()}
             >
               Restart
@@ -146,6 +147,19 @@ const Home = () => {
           </div>
         )}
       </div>
+      <p className="absolute bottom-0 justify-center w-full text-center text-gray-500">
+        Feito com 💙 pelo{' '}
+        <a
+          href="https://matheuschiodi.github.io/Portfolio/"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="transition-colors hover:text-blue-500"
+        >
+          <span className="font-bold text-blue-500">&lt;</span>
+          Matheus Chiodi{' '}
+          <span className="font-bold text-blue-500">/&gt;</span>
+        </a>
+      </p>
     </>
   );
 };
